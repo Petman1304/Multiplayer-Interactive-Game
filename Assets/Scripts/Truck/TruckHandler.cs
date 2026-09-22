4aed4c661771ce00f4ff5223290e2857af76b166
petrus_nm <petrusnmanurung@gmail.com>
2026-09-22 17:07:28 +0700

    feat driver core : add follow camera

--- Assets/Scripts/Truck/TruckHandler.cs
+++ Assets/Scripts/Truck/TruckHandler.cs
@@ -8,12 +8,23 @@ public class TruckHandler : MonoBehaviour
     Rigidbody rb;
 
     [SerializeField]
+    Transform truckModel;
+
+    [SerializeField]
     float truckAcceleration = 50;
 
     [SerializeField]
     float steerInputMultiplier = 100;
 
+    [SerializeField]
+    float maxSteerVelocity = 10;
+
     Vector2 input = Vector2.zero;
+
+    private void Update()
+    {
+        truckModel.transform.rotation = Quaternion.Euler(0, rb.linearVelocity.x * 0.25f, 0);
+    }
 
     private void FixedUpdate()
     {
@@ -33,6 +44,16 @@ public class TruckHandler : MonoBehaviour
         if(Mathf.Abs(input.x) > 0)
         {
             rb.AddForce(rb.transform.right * steerInputMultiplier * input.x);
+
+            float normalizedX = rb.linearVelocity.x / maxSteerVelocity;
+
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+
+            rb.linearVelocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.linearVelocity.z);
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, new Vector3(0, 0, rb.linearVelocity.z), Time.fixedDeltaTime * 3);
         }
     }
 
